641c618eb50a6eaae46b41affac4360055c97abf
Ayagoumi <agoumi94@gmail.com>
2022-04-16 16:07:51 +0000

    💄 added app state

--- App.jsx
+++ App.jsx
@@ -3,34 +3,31 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from "./Views/Login";
 import Home from "./Views/Home";
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import AnimatedSplash from "react-native-animated-splash-screen";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import Feather from "react-native-vector-icons/Feather";
-import ProteinDetail from "./components/ProteinDetail";
+import { StyleSheet, AppState } from "react-native";
+import Ligand from "./components/Ligand";
+import { navigationRef } from './components/RootNavigation';
 
 const Stack = createNativeStackNavigator();
 
-const Ligand = ({ navigation, route }) => {
-  return (
-    <View style={{ overflow: "hidden", position: "relative", alignItems: "center", width: "100%", height: "100%" }}>
-      <View style={styles.header}>
-        <View style={styles.dissmisable}></View>
-        <TouchableOpacity onPress={() => { navigation.goBack() }} style={styles.headerIcon}>
-          <Feather name="chevron-left" size={25} color="#0078ff" />
-        </TouchableOpacity>
-        <Text style={styles.headerTitle}>{route.params.name}</Text>
-      </View>
-      <View style={styles.content}>
-        <Text style={{ color: "#fff" }}>Ligand</Text>
-      </View>
-      <ProteinDetail atom="C" />
-    </View>
-  )
-}
-
 export default function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const appState = useRef(AppState.currentState);
+
+  useEffect(() => {
+    AppState.addEventListener('change', handleAppStateChange);
+    return () => {
+      AppState.removeEventListener('change', handleAppStateChange);
+    };
+  }, []);
+
+  const handleAppStateChange = (nextAppState) => {
+    if (nextAppState.match(/inactive|background/) && appState.current === 'active')
+      console.log(navigationRef.current.navigate('Login'));
+
+    appState.current = nextAppState;
+  };
 
   useEffect(() => {
     (async () => {
@@ -50,21 +47,28 @@ export default function App() {
       logoHeight={250}
       logoWidth={250}
     >
-      <NavigationContainer>
+      <NavigationContainer ref={navigationRef}>
         <Stack.Navigator>
           <Stack.Screen
             name="Login"
             component={Login}
             options={{ headerShown: false }}
           />
-          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-          <Stack.Group screenOptions={{ presentation: 'modal' }}>
-            <Stack.Screen
-              name="Ligand"
-              component={Ligand}
-              options={{ headerShown: false }}
-            />
-          </Stack.Group>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          {/* <Stack.Group screenOptions={{ presentation: 'modal', gestureEnabled: false, customAnimationOnGesture: false }}> */}
+          <Stack.Screen
+            name="Ligand"
+            component={Ligand}
+            // options={{ headerShown: false }}
+            options={({ route }) => ({
+              title: route.params.name,
+            })}
+          />
+          {/* </Stack.Group> */}
         </Stack.Navigator>
       </NavigationContainer>
     </AnimatedSplash>
